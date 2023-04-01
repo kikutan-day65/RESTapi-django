0f6b6923ce5fc744ab42043530b5807df775c4cf
Haruto Mori <baystarsman@outlook.jp>
2023-04-01 23:13:17 +0200

    see model instance to dictionary

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,6 +5,7 @@ import requests
 endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"}) # HTTP Reuqest
+# print(get_response.headers)
 # print(get_response.text)    # print raw text response
 # print(get_response.status_code)
 
